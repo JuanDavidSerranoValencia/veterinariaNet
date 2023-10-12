dec0ea3b7ec4027604371ceadcc13a40e4031915
JuanDavidSerranoValencia <serranovalenciajuandavid949@gmail.com>
2023-10-11 22:37:33 -0500

    feat:Referencia Entities en Context

--- Infrastructure/Data/VeterinariaNetContext.cs
+++ Infrastructure/Data/VeterinariaNetContext.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Reflection;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Core.Entitites;
+
 
 
 namespace Infrastructure.Data;
@@ -15,6 +17,17 @@ public class VeterinariaNetContext : DbContext
     public VeterinariaNetContext(DbContextOptions options) : base(options)
     {
     }
+
+    public DbSet<Cliente> Clientes {get;set;}
+    public DbSet<Cita> Citas {get;set;}
+    public DbSet<Ciudad> Ciudades {get;set;}
+    public DbSet<ClienteDireccion> ClientesDirecciones {get;set;}
+    public DbSet<ClienteTelefono> ClientesTelefonos {get;set;}
+    public DbSet<Departamento> Departamentos {get;set;}
+    public DbSet<Mascota> Mascotas {get;set;}
+    public DbSet<Pais> Paises {get;set;}
+    public DbSet<Raza> Razas {get;set;}
+    public DbSet<Servicio> Servicios {get;set;}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
